6d542443b4dd5a2247b49d457a7e0a07dbac5166
LilDaddySalad <pavel_sushko@yahoo.com>
2021-01-12 00:12:09 -0500

    test 6

--- autoMerge.ps1
+++ autoMerge.ps1
@@ -3,45 +3,42 @@
     $i = 1
     [string]$username = "Pavel-Sushko"
 
-    [Parameter (Mandatory=$false)] [string]$a
-    [Parameter (Mandatory=$false)] [string]$r
-    [Parameter (Mandatory=$false)] [string]$m
+    [Parameter (Mandatory=$false)] [string]$asignee
+    [Parameter (Mandatory=$false)] [string]$reviewer
+    [Parameter (Mandatory=$false)] [string]$merge
 
     if(-not($PSBoundParameters.ContainsKey('assignee')) -and $a)
     {
-        $a = $username
+        $asignee = $username
     }
 
-    if(-not($PSBoundParameters.ContainsKey('reviewer')) -and $r)
+    if(-not($PSBoundParameters.ContainsKey('reviewer')) -and $reviewer)
     {
-        $r = $username
+        $reviewer = $username
     }
 
-    if(-not($PSBoundParameters.ContainsKey('merge')) -and $m)
+    if(-not($PSBoundParameters.ContainsKey('merge')) -and $merge)
     {
-        $m = $false
+        $merge = $false
     }
     else
     {
-        $m = $true
+        $merge = $true
     }
 
     do
     {
         [string]$checkStr = $(git "$username-patch-$i" 2>&1)
 
-        echo "In loop iteration $i"
-        echo "$a-patch-$i"
+        [string]$branch = "$asignee-patch-$i"
 
-        if ($checkStr -eq "fatal: A branch named '$a-patch-$i' already exists.")
+        if ($checkStr -eq "fatal: A branch named '$branch' already exists.")
         {
             $check = $false
         }
         else
         {
             $check = $true
-
-            [string]$branch = "$username-patch-$i"
 
             git branch $branch
         }
@@ -56,7 +53,7 @@
 
     git push --all --progress
 
-    gh pr create -f -a $a
+    gh pr create -f -a $a -r $reviewer
 
     if ($m)
     {
